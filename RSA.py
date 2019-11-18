d4be318e07f65faee8b302a987a0ae424bd0c26d
luengpaul <luengpaul1127@gmail.com>
2019-11-18 15:41:21 -0800

    Finished 2 steps out of 5 step RSA Algorithm

---
+++ RSA.py
@@ -0,0 +1,12 @@
+print("Step 1")
+p=int(input("Please Enter a prime number 'P' value: "))
+q=int(input("Please Enter a prime number 'Q' value: "))
+print("Step 2")
+n=p*q
+print("Your n value is: ")
+print(n)
+print("Your Phi value is ")
+phi=(p-1)*(q-1)
+print(phi)
+print("Step 3")
+
