169c405154818e624caf78775580352422adcf12
luengpaul <luengpaul1127@gmail.com>
2019-12-02 16:27:58 -0800

    Added Steps 1-4 of RSA Algorithm

--- RSA.py
+++ RSA.py
@@ -8,5 +8,39 @@ print(n)
 print("Your Phi value is ")
 phi=(p-1)*(q-1)
 print(phi)
+phi_2=phi
 print("Step 3")
+
+
+def __gcd(a, b):
+    # Everything divides 0
+    if (a == 0 or b == 0): return 0
+
+    # base case
+    if (a == b): return a
+
+    # a is greater
+    if (a > b):
+        return __gcd(a - b, b)
+
+    return __gcd(a, b - a)
+
+
+def coprime(a, b):
+    if (__gcd(a, b) == 1):
+        return a
+
+for x in range(phi):
+    if(coprime(x,phi_2)!=p and coprime(x,phi_2)!=q ):
+        e= coprime(x,phi_2)
+
+print("Your e value is ")
+print(e)
+
+print("Step 4")
+
+
+
+
+
 
